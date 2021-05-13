162535e4ddd130da9b299786ccf40bdb11ae9465
Amakris12 <71471809+Amakris12@users.noreply.github.com>
2021-05-12 18:18:01 -0700

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,1 +1,43 @@
 //Javascript
+$('#Sunday').click(function(){
+    $('body').css('background-color','')
+    $('.accordion-body').css('background-color','#006BEE')
+    $('h3').css('color','white')
+    $('p').css('color','white')
+})
+$('#Monday').click(function(){
+    $('body').css('background-color','')
+    $('.accordion-body').css('background-color','#006BEE')
+    $('h3').css('color','white')
+    $('p').css('color','white')
+})
+$('#tuesday').click(function(){
+    $('body').css('background-color','')
+    $('.accordion-body').css('background-color','#006BEE')
+    $('h3').css('color','white')
+    $('p').css('color','white')
+})
+$('#wensday').click(function(){
+    $('body').css('background-color','')
+    $('.accordion-body').css('background-color','#006BEE')
+    $('h3').css('color','white')
+    $('p').css('color','white')
+})
+$('#thursday').click(function(){
+    $('body').css('background-image','')
+    $('.accordion-body').css('background-color','#006BEE')
+    $('h3').css('color','white')
+    $('p').css('color','white')
+})
+$('#friday').click(function(){
+    $('body').css('background-color','')
+    $('.accordion-body').css('background-color','#006BEE')
+    $('h3').css('color','white')
+    $('p').css('color','white')
+})
+$('#Saturday').click(function(){
+    $('body').css('background-image','')
+    $('.accordion-body').css('background-color','#006BEE')
+    $('h3').css('color','white')
+    $('p').css('color','white')
+})
